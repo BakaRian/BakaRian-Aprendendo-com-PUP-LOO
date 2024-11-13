d4d867a8f086700434d2d89ec8e827638990e10a
Rian Fernandes <151675874+BakaRian@users.noreply.github.com>
2024-11-12 21:08:59 -0300

    Add files via upload

---
+++ Js/script-tag.js
@@ -0,0 +1,9 @@
+    // Função para parar o marquee
+    function stopMarquee() {
+        document.getElementById("myMarquee").stop();
+    }
+
+    // Função para iniciar o marquee
+    function startMarquee() {
+        document.getElementById("myMarquee").start();
+    }
